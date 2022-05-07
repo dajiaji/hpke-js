dfcc36a975f9b341773a57020464d7bcc71babcf
Ajitomi Daisuke <ajitomi@gmail.com>
2022-05-07 09:15:43 +0900

    Refine Jest config.

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,19 +1,12 @@
-import yn from "yn";
-
-const collectCoverage = yn(process.env.CI);
-
 export default {
-    preset: "ts-jest",
-    clearMocks: true,
-    // setupFilesAfterEnv: ["./test/setup.ts"],
-    testMatch: ["**/{src,test}/**/*.test.ts"],
-    testEnvironment: "jsdom",
-    collectCoverage,
-    coverageReporters: collectCoverage ? ["lcov"] : ["lcov", "text"],
-    globals: {
-        "ts-jest": {
-            // skip ts-jest type checking, incremental compilation with tsc is much faster
-            isolatedModules: true,
-        },
+  preset: 'ts-jest',
+  clearMocks: true,
+  testMatch: ['**/test/**/*.test.ts'],
+  testEnvironment: 'jsdom',
+  coverageReporters: ['lcov', 'text'],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
     },
+  },
 };
