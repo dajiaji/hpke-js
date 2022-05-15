b0eb95537abb7f14850e067c4fbcbb74ec513cb7
Ajitomi Daisuke <ajitomi@gmail.com>
2022-05-16 05:23:10 +0900

    Refine public interfaces.

--- src/mod.ts
+++ src/mod.ts
@@ -1,16 +1,15 @@
-export * from './errors';
-
 export type { CipherSuiteParams } from './interfaces/cipherSuiteParams';
-export type { CipherSuiteSealResponse } from './interfaces/responses';
-export type { RecipientContextParams } from './interfaces/recipientContextParams';
-export type { SenderContextParams } from './interfaces/senderContextParams';
-export type { PreSharedKey } from './interfaces/preSharedKey';
 export type {
+  EncryptionContextInterface,
   RecipientContextInterface,
   SenderContextInterface,
 } from './interfaces/encryptionContextInterface';
+export type { PreSharedKey } from './interfaces/preSharedKey';
+export type { RecipientContextParams } from './interfaces/recipientContextParams';
+export type { CipherSuiteSealResponse } from './interfaces/responses';
+export type { SenderContextParams } from './interfaces/senderContextParams';
+
+export * from './errors';
 
 export { Kem, Kdf, Aead } from './identifiers';
 export { CipherSuite } from './cipherSuite';
-export { SenderContext } from './senderContext';
-export { RecipientContext } from './recipientContext';
