51c2a0c5f8dea240d76e018f94d8216548800944
Ajitomi Daisuke <ajitomi@gmail.com>
2022-05-16 22:41:30 +0900

    Add typedoc definition.

--- src/identifiers.ts
+++ src/identifiers.ts
@@ -1,3 +1,6 @@
+/**
+ * Supported HPKE modes.
+ */
 export enum Mode {
   Base = 0x00,
   Psk = 0x01,
@@ -5,23 +8,47 @@ export enum Mode {
   AuthPsk = 0x03,
 }
 
+/**
+ * Supported Key Encapsulation Mechanisms (KEMs).
+ */
 export enum Kem {
+  /** DHKEM (P-256, HKDF-SHA256). */
   DhkemP256HkdfSha256 = 0x0010,
+  /** DHKEM (P-384, HKDF-SHA384). */
   DhkemP384HkdfSha384 = 0x0011,
+  /** DHKEM (P-521, HKDF-SHA512). */
   DhkemP521HkdfSha512 = 0x0012,
+  // /** DHKEM (X25519, HKDF-SHA256) */
   // DhkemX25519HkdfSha256 = 0x0020,
+  // /** DHKEM (X448, HKDF-SHA512) */
   // DhkemX448HkdfSha512 = 0x0021,
 }
 
+/**
+ * Supported Key Derivation Functions (KDFs).
+ */
 export enum Kdf {
+  /** HKDF-SHA256. */
   HkdfSha256 = 0x0001,
+  /** HKDF-SHA384. */
   HkdfSha384 = 0x0002,
+  /** HKDF-SHA512. */
   HkdfSha512 = 0x0003,
 }
 
+/**
+ * Supported Authenticated Encryption with Associated Data (AEAD) Functions.
+ */
 export enum Aead {
+  /** AES-128-GCM. */
   Aes128Gcm = 0x0001,
+  /** AES-256-GCM. */
   Aes256Gcm = 0x0002,
+  // /** ChaCha20Poly1305. */
   // ChaCha20Poly1305 = 0x0003,
+  /**
+   * Export-only mode for applications that only use the export() function
+   * to get secrets for AEAD.
+   */
   ExportOnly = 0xFFFF,
 }
--- src/interfaces/exporter.ts
+++ src/interfaces/exporter.ts
@@ -1,12 +1,15 @@
 /**
  * The exporter interface.
- *
- * @public
  */
 export interface Exporter {
 
   /**
    * Exports a secret using a variable-length pseudorandom function.
+   *
+   * @param info An exporter context string as bytes.
+   * @param len A desired length in bytes of the output secret.
+   * @returns A secret string as bytes.
+   * @throws {@link ExportError}
    */
   export(info: ArrayBuffer, len: number): Promise<ArrayBuffer>;
 }
