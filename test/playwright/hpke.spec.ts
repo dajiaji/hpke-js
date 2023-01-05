3522e72131a45a9eaf91f2ad741964febae712bf
Ajitomi Daisuke <ajitomi@gmail.com>
2023-01-05 10:44:54 +0900

    Add playwrite test for secp256k1.

--- test/playwright/hpke.spec.ts
+++ test/playwright/hpke.spec.ts
@@ -4,6 +4,6 @@ test("basic test", async ({ page }) => {
   await page.goto("https://dajiaji.github.io/hpke-js/");
   await page.click("text=run");
   await page.waitForTimeout(5000);
-  await expect(page.locator("id=pass")).toHaveText("45");
+  await expect(page.locator("id=pass")).toHaveText("54");
   await expect(page.locator("id=fail")).toHaveText("0");
 });
