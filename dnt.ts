b628ab05a1f38aadd2a59a1b4e4d90732f7ac142
Ajitomi Daisuke <ajitomi@gmail.com>
2023-01-16 21:11:59 +0900

    Disable typeCheck for npm package having sub-path.

--- dnt.ts
+++ dnt.ts
@@ -5,7 +5,7 @@ await emptyDir("./npm");
 await build({
   entryPoints: ["./mod.ts"],
   outDir: "./npm",
-  typeCheck: true,
+  typeCheck: false,
   test: true,
   declaration: true,
   scriptModule: "umd",
