e2a3e3f2b244962195156c6f986a5f97fa9efffc
Ajitomi Daisuke <ajitomi@gmail.com>
2023-01-16 19:08:57 +0900

    Use audited secp256k1 lib.

--- src/kems/dhkemPrimitives/secp256k1.ts
+++ src/kems/dhkemPrimitives/secp256k1.ts
@@ -1,24 +1,20 @@
-import * as elliptic from "elliptic";
+import * as secp from "secp256k1";
 
 import type { KemPrimitives } from "../../interfaces/kemPrimitives.ts";
 import type { KdfInterface } from "../../interfaces/kdfInterface.ts";
 
-import { loadCrypto } from "../../webCrypto.ts";
 import { XCryptoKey } from "../../xCryptoKey.ts";
-import { bytesToHex, hexToBytes } from "../../utils/misc.ts";
 
 import * as consts from "../../consts.ts";
 
 const ALG_NAME = "ECDH";
 
 export class Secp256K1 implements KemPrimitives {
-  private _ec: elliptic.EC;
   private _hkdf: KdfInterface;
   private _nPk: number;
   private _nSk: number;
 
   constructor(hkdf: KdfInterface) {
-    this._ec = new elliptic.EC("secp256k1");
     this._hkdf = hkdf;
     this._nPk = 65;
     this._nSk = 32;
@@ -48,10 +44,8 @@ export class Secp256K1 implements KemPrimitives {
   }
 
   public async generateKeyPair(): Promise<CryptoKeyPair> {
-    const raw = new Uint8Array(32);
-    const cryptoApi = await loadCrypto();
-    cryptoApi.getRandomValues(raw);
-    const sk = new XCryptoKey(ALG_NAME, raw, "private");
+    const rawSk = secp.utils.randomPrivateKey();
+    const sk = new XCryptoKey(ALG_NAME, rawSk, "private");
     const pk = await this.derivePublicKey(sk);
     return { publicKey: pk, privateKey: sk };
   }
@@ -115,24 +109,15 @@ export class Secp256K1 implements KemPrimitives {
 
   private _derivePublicKey(k: XCryptoKey): Promise<CryptoKey> {
     return new Promise((resolve) => {
-      const kp = this._ec.keyFromPrivate(bytesToHex(k.key), "hex");
-      resolve(
-        new XCryptoKey(
-          ALG_NAME,
-          hexToBytes(kp.getPublic("hex")),
-          "public",
-        ),
-      );
+      const pk = secp.getPublicKey(k.key);
+      resolve(new XCryptoKey(ALG_NAME, pk, "public"));
     });
   }
 
   private _dh(sk: XCryptoKey, pk: XCryptoKey): Promise<ArrayBuffer> {
     return new Promise((resolve, reject) => {
       try {
-        const skp = this._ec.keyFromPrivate(bytesToHex(sk.key), "hex");
-        const rkp = this._ec.keyFromPublic(bytesToHex(pk.key), "hex");
-        const ret = skp.derive(rkp.getPublic());
-        resolve(hexToBytes(ret.toString(16, 2)));
+        resolve(secp.getSharedSecret(sk.key, pk.key).buffer);
       } catch (e: unknown) {
         reject(e);
       }
