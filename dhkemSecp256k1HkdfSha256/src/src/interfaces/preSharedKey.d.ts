cb129e1911af55606334404142030fbbb4a0f697
dajiaji <dajiaji@users.noreply.github.com>
2023-07-26 21:33:01 +0000

    deploy: e1b49251eec459a5dc1fede7c37d461838626c86

--- dhkemSecp256k1HkdfSha256/src/src/interfaces/preSharedKey.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The pre-shared key interface.
- */
-export interface PreSharedKey {
-    /** The key identifier. */
-    id: ArrayBuffer;
-    /** The body of the pre-shared key. */
-    key: ArrayBuffer;
-}
--- dhkemSecp256k1HkdfSha256/src/src/xCryptoKey.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class XCryptoKey implements CryptoKey {
-    readonly key: Uint8Array;
-    readonly type: "public" | "private";
-    readonly extractable: boolean;
-    readonly algorithm: KeyAlgorithm;
-    readonly usages: KeyUsage[];
-    constructor(name: string, key: Uint8Array, type: "public" | "private");
-}
