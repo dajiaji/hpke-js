ec6a1604e767a11e5c13bc44f47a6a080bc1c7ba
Ajitomi Daisuke <dajiaji@gmail.com>
2023-08-19 01:53:54 +0900

    Add js runtime test for HybridkemX25519Kyber768.

---
+++ x/hybridkem-x25519-kyber768/test/runtimes/server.js
@@ -0,0 +1,103 @@
+import {
+  AeadId,
+  Aes128Gcm,
+  Aes256Gcm,
+  CipherSuite,
+  // DhkemP256HkdfSha256,
+  // DhkemP384HkdfSha384,
+  // DhkemP521HkdfSha512,
+  ExportOnly,
+  HkdfSha256,
+  HkdfSha384,
+  HkdfSha512,
+  KdfId,
+} from "./hpke-core.js";
+
+import { HybridkemX25519Kyber768 } from "./hpke-hybridkem-x25519-kyber768.js";
+
+// function createKem(id) {
+//   switch (id) {
+//     case KemId.DhkemP256HkdfSha256:
+//       return new DhkemP256HkdfSha256();
+//     case KemId.DhkemP384HkdfSha384:
+//       return new DhkemP384HkdfSha384();
+//     case KemId.DhkemP521HkdfSha512:
+//       return new DhkemP521HkdfSha512();
+//     default:
+//       break;
+//   }
+//   throw new Error("ng: invalid kem");
+// }
+
+function createKdf(id) {
+  switch (id) {
+    case KdfId.HkdfSha256:
+      return new HkdfSha256();
+    case KdfId.HkdfSha384:
+      return new HkdfSha384();
+    case KdfId.HkdfSha512:
+      return new HkdfSha512();
+    default:
+      break;
+  }
+  throw new Error("ng: invalid kdf");
+}
+
+function createAead(id) {
+  switch (id) {
+    case AeadId.Aes128Gcm:
+      return new Aes128Gcm();
+    case AeadId.Aes256Gcm:
+      return new Aes256Gcm();
+    // case AeadId.Chacha20Poly1305:
+    //   return new Chacha20Poly1305();
+    case AeadId.ExportOnly:
+      return new ExportOnly();
+    default:
+      break;
+  }
+  throw new Error("ng: invalid aead");
+}
+
+export async function testServer(request) {
+  const url = new URL(request.url);
+  if (url.pathname !== "/test") {
+    return new Response("ng: invalid path");
+  }
+  const params = url.searchParams;
+  const kdfStr = params.get("kdf");
+  const aeadStr = params.get("aead");
+  if (kdfStr === null || aeadStr === null) {
+    return new Response("ng: invalid params");
+  }
+  const kem = new HybridkemX25519Kyber768();
+  const kdf = Number.parseInt(kdfStr);
+  const aead = Number.parseInt(aeadStr);
+  if (Number.isNaN(kdf) || Number.isNaN(aead)) {
+    return new Response("ng: invalid params");
+  }
+
+  try {
+    const suite = new CipherSuite({
+      kem: kem,
+      kdf: createKdf(kdf),
+      aead: createAead(aead),
+    });
+    const rkp = await suite.kem.generateKeyPair();
+    const sender = await suite.createSenderContext({
+      recipientPublicKey: rkp.publicKey,
+    });
+    const recipient = await suite.createRecipientContext({
+      recipientKey: rkp,
+      enc: sender.enc,
+    });
+    const ct = await sender.seal(new TextEncoder().encode("hello world!"));
+    const pt = await recipient.open(ct);
+    if ("hello world!" !== new TextDecoder().decode(pt)) {
+      return new Response("ng");
+    }
+  } catch (e) {
+    return new Response("ng: " + e.message);
+  }
+  return new Response("ok");
+}
