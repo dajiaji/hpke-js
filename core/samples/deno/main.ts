32e21909dc5b4b048b29c4ec7bab644dc6259c0c
Ajitomi Daisuke <dajiaji@gmail.com>
2024-08-14 09:04:57 +0900

    Add samples/deno for @hpke/core.

---
+++ core/samples/deno/main.ts
@@ -0,0 +1,38 @@
+import {
+  Aes128Gcm, CipherSuite, DhkemP256HkdfSha256, HkdfSha256,
+} from "@hpke/core";
+
+async function doHpke() {
+  // setup
+  const suite = new CipherSuite({
+    kem: new DhkemP256HkdfSha256(),
+    kdf: new HkdfSha256(),
+    aead: new Aes128Gcm(),
+  });
+
+  const rkp = await suite.kem.generateKeyPair();
+
+  const sender = await suite.createSenderContext({
+    recipientPublicKey: rkp.publicKey,
+  });
+
+  const recipient = await suite.createRecipientContext({
+    recipientKey: rkp.privateKey,
+    enc: sender.enc,
+  });
+
+  // encrypt
+  const ct = await sender.seal(new TextEncoder().encode("Hello world!"));
+
+  // decrypt
+  const pt = await recipient.open(ct);
+
+  // Hello world!
+  console.log(new TextDecoder().decode(pt));
+}
+
+try {
+  doHpke();
+} catch (_err: unknown) {
+  console.log("doHPKE() failed.");
+}
