1f967fe7e3440c49fc528d3f4b74e9d45866954c
Ajitomi Daisuke <dajiaji@gmail.com>
2024-08-15 17:48:23 +0900

    Add samples/deno to x/chacha20poly1305.

---
+++ x/chacha20poly1305/samples/deno/main.ts
@@ -0,0 +1,37 @@
+import { CipherSuite, DhkemP256HkdfSha256, HkdfSha256 } from "@hpke/core";
+import { Chacha20Poly1305 } from "@hpke/chacha20poly1305";
+
+async function doHpke() {
+  // setup
+  const suite = new CipherSuite({
+    kem: new DhkemP256HkdfSha256(),
+    kdf: new HkdfSha256(),
+    aead: new Chacha20Poly1305(),
+  });
+
+  const rkp = await suite.kem.generateKeyPair();
+
+  const sender = await suite.createSenderContext({
+    recipientPublicKey: rkp.publicKey,
+  });
+
+  const recipient = await suite.createRecipientContext({
+    recipientKey: rkp.privateKey,
+    enc: sender.enc,
+  });
+
+  // encrypt
+  const ct = await sender.seal(new TextEncoder().encode("Hello world!"));
+
+  // decrypt
+  const pt = await recipient.open(ct);
+
+  // Hello world!
+  console.log(new TextDecoder().decode(pt));
+}
+
+try {
+  doHpke();
+} catch (_err: unknown) {
+  console.log("doHPKE() failed.");
+}
