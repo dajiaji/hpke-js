1644015119b22eded3c79be4bde077eeda20236e
Ajitomi Daisuke <dajiaji@gmail.com>
2024-08-25 12:56:51 +0900

    Refine public functions for core.

--- x/core/mod.ts
+++ x/core/mod.ts
@@ -1,8 +1,6 @@
 export type { AeadEncryptionContext } from "./src/interfaces/aeadEncryptionContext.ts";
 export type { AeadInterface } from "./src/interfaces/aeadInterface.ts";
 export type { CipherSuiteParams } from "./src/interfaces/cipherSuiteParams.ts";
-export type { DhkemPrimitives } from "./src/interfaces/dhkemPrimitives.ts";
-export type { DhkemInterface } from "./src/interfaces/dhkemInterface.ts";
 export type {
   EncryptionContext,
   RecipientContext,
@@ -15,33 +13,11 @@ export type { RecipientContextParams } from "./src/interfaces/recipientContextPa
 export type { CipherSuiteSealResponse } from "./src/interfaces/responses.ts";
 export type { SenderContextParams } from "./src/interfaces/senderContextParams.ts";
 
-export {
-  KEM_USAGES,
-  LABEL_DKP_PRK,
-  LABEL_SK,
-} from "./src/interfaces/dhkemPrimitives.ts";
-export { SUITE_ID_HEADER_KEM } from "./src/interfaces/kemInterface.ts";
-
 export { Aes128Gcm, Aes256Gcm } from "./src/aeads/aesGcm.ts";
 export { ExportOnly } from "./src/aeads/exportOnly.ts";
-export {
-  // HkdfSha256Native,
-  HkdfSha384Native,
-  HkdfSha512Native,
-} from "./src/kdfs/hkdf.ts";
-export { Dhkem } from "./src/kems/dhkem.ts";
-export { Ec } from "./src/kems/dhkemPrimitives/ec.ts";
-export { Hybridkem } from "./src/kems/hybridkem.ts";
-export {
-  base64UrlToBytes,
-  concat,
-  i2Osp,
-  isCryptoKeyPair,
-} from "./src/utils/misc.ts";
-
-export { INPUT_LENGTH_LIMIT } from "./src/consts.ts";
 export * from "./src/errors.ts";
 export { AeadId, KdfId, KemId } from "./src/identifiers.ts";
+
 export {
   CipherSuite,
   DhkemP256HkdfSha256,
@@ -51,4 +27,14 @@ export {
   HkdfSha384,
   HkdfSha512,
 } from "./src/native.ts";
+
+// Followings are disclosed for hpke-js family modules
+export type { DhkemInterface } from "./src/interfaces/dhkemInterface.ts";
+export type { DhkemPrimitives } from "./src/interfaces/dhkemPrimitives.ts";
+
+export { Dhkem } from "./src/kems/dhkem.ts";
+export { Ec } from "./src/kems/dhkemPrimitives/ec.ts";
+export { Hybridkem } from "./src/kems/hybridkem.ts";
 export { XCryptoKey } from "./src/xCryptoKey.ts";
+
+export { INPUT_LENGTH_LIMIT } from "./src/consts.ts";
