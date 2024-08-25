d4168b0f3d8575f0c1c80896befc47a2b30c0c0d
Ajitomi Daisuke <dajiaji@gmail.com>
2024-08-25 14:58:56 +0900

    Fix path to deprecated Aead, Kdf and Kem.

--- x/hpke-js/mod.ts
+++ x/hpke-js/mod.ts
@@ -33,6 +33,6 @@ export {
   ValidationError,
 } from "@hpke/core";
 
-export { Aead, Kdf, Kem } from "../../x/core/src/identifiers.ts";
+export { Aead, Kdf, Kem } from "../core/src/identifiers.ts";
 
 export { CipherSuite } from "./src/cipherSuite.ts";
