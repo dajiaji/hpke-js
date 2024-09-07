e2f721111f08bea3c491844b5fd6e0a60b7d0e6f
Ajitomi Daisuke <dajiaji@gmail.com>
2024-09-07 18:41:06 +0900

    Refine dhkem-secp256k1/test for cloudflare.

--- packages/dhkem-secp256k1/test/runtimes/cloudflare/dhkem-secp256k1.spec.ts
+++ packages/dhkem-secp256k1/test/runtimes/cloudflare/dhkem-secp256k1.spec.ts
@@ -1,15 +1,16 @@
-import { assertEquals } from "@std/assert";
-import { describe, it } from "@std/testing/bdd";
+import { SELF } from "cloudflare:test";
+import { describe, expect, it } from "vitest";
 
 describe("Cloudflare Workers", () => {
   describe("GET /test", () => {
     it("should return ok", async () => {
       for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
         for (const aead of ["0x0001", "0x0002"]) {
-          const res = await fetch(
-            `http://localhost:8788/test?kdf=${kdf}&aead=${aead}`,
+          const res = await SELF.fetch(
+            `https://example.com/test?kdf=${kdf}&aead=${aead}`,
           );
-          assertEquals("ok", await res.text());
+          expect(res.status).toBe(200);
+          expect(await res.text()).toBe("ok");
         }
       }
     });
