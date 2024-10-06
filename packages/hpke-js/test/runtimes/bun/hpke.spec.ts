329d072f2b38a5fc0d7a8326332bf8e318ac100c
Ajitomi Daisuke <dajiaji@gmail.com>
2024-10-06 11:46:22 +0900

    Use bun test.

--- packages/hpke-js/test/runtimes/bun/hpke.spec.ts
+++ packages/hpke-js/test/runtimes/bun/hpke.spec.ts
@@ -1,82 +1,28 @@
-import { assertEquals } from "@std/assert";
-import { describe, it } from "@std/testing/bdd";
+import { expect, test } from "bun:test";
 
-describe("Bun", () => {
-  describe("GET /test?kem=0x0010", () => {
-    it("should return ok", async () => {
-      for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-        for (const aead of ["0x0001", "0x0002", "0x0003"]) {
-          const res = await fetch(
-            `http://localhost:3000/test?kem=0x0010&kdf=${kdf}&aead=${aead}`,
-          );
-          assertEquals("ok", await res.text());
+import { CipherSuite } from "hpke-js";
+
+test("bun - normal cases", async () => {
+  for (const kem of [0x0010, 0x0011, 0x0012, 0x0020, 0x0021]) {
+    for (const kdf of [0x0001, 0x0002, 0x0003]) {
+      for (const aead of [0x0001, 0x0002, 0x0003]) {
+        try {
+          const suite = new CipherSuite({ kem: kem, kdf: kdf, aead: aead });
+          const rkp = await suite.kem.generateKeyPair();
+          const sender = await suite.createSenderContext({
+            recipientPublicKey: rkp.publicKey,
+          });
+          const recipient = await suite.createRecipientContext({
+            recipientKey: rkp,
+            enc: sender.enc,
+          });
+          const ct = await sender.seal(new TextEncoder().encode("hello world!"));
+          const pt = await recipient.open(ct);
+          expect(new TextDecoder().decode(pt)).toBe("hello world!");
+        } catch (e: unknown) {
+          expect().fail("ng: " + (e as Error).message);
         }
       }
-    });
-  });
-
-  describe("GET /test?kem=0x0011", () => {
-    it("should return ok", async () => {
-      for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-        for (const aead of ["0x0001", "0x0002", "0x0003"]) {
-          const res = await fetch(
-            `http://localhost:3000/test?kem=0x0011&kdf=${kdf}&aead=${aead}`,
-          );
-          assertEquals("ok", await res.text());
-        }
-      }
-    });
-  });
-
-  describe("GET /test?kem=0x0012", () => {
-    it("should return ok", async () => {
-      for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-        for (const aead of ["0x0001", "0x0002", "0x0003"]) {
-          const res = await fetch(
-            `http://localhost:3000/test?kem=0x0012&kdf=${kdf}&aead=${aead}`,
-          );
-          assertEquals("ok", await res.text());
-        }
-      }
-    });
-  });
-
-  // describe("GET /test?kem=0x0013", () => {
-  //   it("should return ok", async () => {
-  //     for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-  //       for (const aead of ["0x0001", "0x0002", "0x0003"]) {
-  //         const res = await fetch(
-  //           `http://localhost:3000/test?kem=0x0013&kdf=${kdf}&aead=${aead}`,
-  //         );
-  //         assertEquals("ok", await res.text());
-  //       }
-  //     }
-  //   });
-  // });
-
-  describe("GET /test?kem=0x0020", () => {
-    it("should return ok", async () => {
-      for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-        for (const aead of ["0x0001", "0x0002", "0x0003"]) {
-          const res = await fetch(
-            `http://localhost:3000/test?kem=0x0020&kdf=${kdf}&aead=${aead}`,
-          );
-          assertEquals("ok", await res.text());
-        }
-      }
-    });
-  });
-
-  describe("GET /test?kem=0x0021", () => {
-    it("should return ok", async () => {
-      for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-        for (const aead of ["0x0001", "0x0002", "0x0003"]) {
-          const res = await fetch(
-            `http://localhost:3000/test?kem=0x0021&kdf=${kdf}&aead=${aead}`,
-          );
-          assertEquals("ok", await res.text());
-        }
-      }
-    });
-  });
+    }
+  }
 });
