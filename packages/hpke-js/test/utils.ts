6573bdec134f33303d689110e379d184f8f75295
Ajitomi Daisuke <dajiaji@gmail.com>
2024-10-06 16:57:43 +0900

    Move encryptionContext tests using EncryptionContextImpl to @hpke/core.

--- packages/hpke-js/test/utils.ts
+++ packages/hpke-js/test/utils.ts
@@ -6,7 +6,7 @@ export const isNode = () => (globalThis as any).process?.versions?.node != null;
 export function getPath(name: string): string {
   const currentPath = dirname(fromFileUrl(import.meta.url));
   if (isNode()) {
-    return join(currentPath, "../../../", name);
+    return join(currentPath, "../../", name);
   }
   return join(currentPath, name);
 }
