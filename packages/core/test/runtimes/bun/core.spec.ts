ff9c77123887dce7a2350d26b68902a3f92ea01b
Ajitomi Daisuke <dajiaji@gmail.com>
2024-10-06 11:23:50 +0900

    Use bun test.

--- packages/core/test/runtimes/bun/core.spec.ts
+++ packages/core/test/runtimes/bun/core.spec.ts
@@ -1,69 +1,93 @@
-import { assertEquals } from "@std/assert";
-import { describe, it } from "@std/testing/bdd";
+import { expect, test } from "bun:test";
 
-describe("Bun", () => {
-  describe("GET /test?kem=0x0010", () => {
-    it("should return ok", async () => {
-      for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-        for (const aead of ["0x0001", "0x0002"]) {
-          const res = await fetch(
-            `http://localhost:3002/test?kem=0x0010&kdf=${kdf}&aead=${aead}`,
-          );
-          assertEquals("ok", await res.text());
+import {
+  AeadId,
+  AeadInterface,
+  Aes128Gcm,
+  Aes256Gcm,
+  CipherSuite,
+  DhkemP256HkdfSha256,
+  DhkemP384HkdfSha384,
+  DhkemP521HkdfSha512,
+  // ExportOnly,
+  HkdfSha256,
+  HkdfSha384,
+  HkdfSha512,
+  KdfId,
+  KdfInterface,
+  KemId,
+  KemInterface,
+} from "@hpke/core";
+
+function createKem(id: number): KemInterface {
+  switch (id) {
+    case KemId.DhkemP256HkdfSha256:
+      return new DhkemP256HkdfSha256();
+    case KemId.DhkemP384HkdfSha384:
+      return new DhkemP384HkdfSha384();
+    case KemId.DhkemP521HkdfSha512:
+      return new DhkemP521HkdfSha512();
+    default:
+      break;
+  }
+  throw new Error("ng: invalid kem");
+}
+
+function createKdf(id: number): KdfInterface {
+  switch (id) {
+    case KdfId.HkdfSha256:
+      return new HkdfSha256();
+    case KdfId.HkdfSha384:
+      return new HkdfSha384();
+    case KdfId.HkdfSha512:
+      return new HkdfSha512();
+    default:
+      break;
+  }
+  throw new Error("ng: invalid kdf");
+}
+
+function createAead(id: AeadId): AeadInterface {
+  switch (id) {
+    case AeadId.Aes128Gcm:
+      return new Aes128Gcm();
+    case AeadId.Aes256Gcm:
+      return new Aes256Gcm();
+    // case AeadId.Chacha20Poly1305:
+    //   return new Chacha20Poly1305();
+    case AeadId.ExportOnly:
+      return new ExportOnly();
+    default:
+      break;
+  }
+  throw new Error("ng: invalid aead");
+}
+
+test("bun - normal cases", async () => {
+  for (const kem of [0x0010, 0x0011, 0x0012]) {
+    for (const kdf of [0x0001, 0x0002, 0x0003]) {
+      for (const aead of [0x0001, 0x0002]) {
+        try {
+          const suite = new CipherSuite({
+            kem: createKem(kem),
+            kdf: createKdf(kdf),
+            aead: createAead(aead),
+          });
+          const rkp = await suite.kem.generateKeyPair();
+          const sender = await suite.createSenderContext({
+            recipientPublicKey: rkp.publicKey,
+          });
+          const recipient = await suite.createRecipientContext({
+            recipientKey: rkp,
+            enc: sender.enc,
+          });
+          const ct = await sender.seal(new TextEncoder().encode("hello world!"));
+          const pt = await recipient.open(ct);
+          expect(new TextDecoder().decode(pt)).toBe("hello world!");
+        } catch (e: unknown) {
+          expect().fail("ng: " + (e as Error).message);
         }
       }
-    });
-  });
-
-  describe("GET /test?kem=0x0011", () => {
-    it("should return ok", async () => {
-      for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-        for (const aead of ["0x0001", "0x0002"]) {
-          const res = await fetch(
-            `http://localhost:3002/test?kem=0x0011&kdf=${kdf}&aead=${aead}`,
-          );
-          assertEquals("ok", await res.text());
-        }
-      }
-    });
-  });
-
-  describe("GET /test?kem=0x0012", () => {
-    it("should return ok", async () => {
-      for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-        for (const aead of ["0x0001", "0x0002"]) {
-          const res = await fetch(
-            `http://localhost:3002/test?kem=0x0012&kdf=${kdf}&aead=${aead}`,
-          );
-          assertEquals("ok", await res.text());
-        }
-      }
-    });
-  });
-
-  // describe("GET /test?kem=0x0020", () => {
-  //   it("should return ok", async () => {
-  //     for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-  //       for (const aead of ["0x0001", "0x0002"]) {
-  //         const res = await fetch(
-  //           `http://localhost:3002/test?kem=0x0020&kdf=${kdf}&aead=${aead}`,
-  //         );
-  //         assertEquals("ok", await res.text());
-  //       }
-  //     }
-  //   });
-  // });
-
-  // describe("GET /test?kem=0x0021", () => {
-  //   it("should return ok", async () => {
-  //     for (const kdf of ["0x0001", "0x0002", "0x0003"]) {
-  //       for (const aead of ["0x0001", "0x0002"]) {
-  //         const res = await fetch(
-  //           `http://localhost:3002/test?kem=0x0021&kdf=${kdf}&aead=${aead}`,
-  //         );
-  //         assertEquals("ok", await res.text());
-  //       }
-  //     }
-  //   });
-  // });
+    }
+  }
 });
