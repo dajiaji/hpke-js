01d8518832aed7b550ba42f41b2e008025b4d802
Ajitomi Daisuke <dajiaji@gmail.com>
2025-08-10 08:12:17 +0900

    core: add DhkemX25519HkdfSha256 test for browsers.

--- packages/core/test/runtimes/browsers/core.spec.ts
+++ packages/core/test/runtimes/browsers/core.spec.ts
@@ -1,9 +1,21 @@
 import { expect, test } from "@playwright/test";
 
 test("basic test", async ({ page }) => {
-  await page.goto("https://dajiaji.github.io/hpke-js/core");
+  await page.goto("./index.html");
   await page.click("text=run");
   await page.waitForTimeout(5000);
   await expect(page.locator("id=pass")).toHaveText("18");
+  await expect(page.locator("id=fail")).toHaveText("0");
+});
+
+test("secure curves test", async ({ browserName, page }) => {
+  test.skip(
+    browserName === "webkit",
+    "Secure curves are not supported in Safari",
+  );
+  await page.goto("./secure_curves.html");
+  await page.click("text=run");
+  await page.waitForTimeout(5000);
+  await expect(page.locator("id=pass")).toHaveText("6");
   await expect(page.locator("id=fail")).toHaveText("0");
 });
