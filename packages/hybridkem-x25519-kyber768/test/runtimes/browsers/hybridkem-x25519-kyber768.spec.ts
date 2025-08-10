ad309d2e85a8b0793bd0a0e7cbbc77dbb619c1c5
Ajitomi Daisuke <dajiaji@gmail.com>
2025-08-10 17:12:20 +0900

    hybridkem-x25519-kyber768: use local http-server for testing on browsers.

--- packages/hybridkem-x25519-kyber768/test/runtimes/browsers/hybridkem-x25519-kyber768.spec.ts
+++ packages/hybridkem-x25519-kyber768/test/runtimes/browsers/hybridkem-x25519-kyber768.spec.ts
@@ -1,9 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("basic test", async ({ page }) => {
-  await page.goto(
-    "https://dajiaji.github.io/hpke-js/hybridkem-x25519-kyber768",
-  );
+  await page.goto("./index.html");
   await page.click("text=run");
   await page.waitForTimeout(5000);
   await expect(page.locator("id=pass")).toHaveText("6");
