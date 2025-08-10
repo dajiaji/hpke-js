869ae44d54c21bbefd52c94c4bb20f3b5c040f54
Ajitomi Daisuke <ajitomi@gmail.com>
2025-08-10 16:32:34 +0900

    chacha20poly1305: use local http-server for testing on browsers. (#554)

--- packages/chacha20poly1305/test/runtimes/browsers/playwright.config.ts
+++ packages/chacha20poly1305/test/runtimes/browsers/playwright.config.ts
@@ -3,6 +3,15 @@ import { devices } from "@playwright/test";
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+  use: {
+    baseURL: "http://localhost:3000",
+  },
+  webServer: {
+    command: "npx http-server ./pages -p 3000 -c-1",
+    url: "http://localhost:3000",
+    // deno-lint-ignore no-process-global
+    reuseExistingServer: !process.env.CI,
+  },
   projects: [
     {
       name: "chromium",
