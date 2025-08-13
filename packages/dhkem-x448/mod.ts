329d99735d9396b2a1eab888905e86cc0bde2fe4
Ajitomi Daisuke <ajitomi@gmail.com>
2025-08-13 12:38:34 +0900

    dhkem-x448: refine directory structure. (#611)

--- packages/dhkem-x448/mod.ts
+++ packages/dhkem-x448/mod.ts
@@ -1,3 +1,2 @@
-export { DhkemX448HkdfSha512 } from "./src/dhkemX448.ts";
+export { DhkemX448HkdfSha512, X448 } from "./src/dhkemX448.ts";
 export { HkdfSha512 } from "./src/hkdfSha512.ts";
-export { X448 } from "./src/x448.ts";
