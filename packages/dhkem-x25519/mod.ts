9ecc06f9300317b11115fbe8e507d826fa871842
Ajitomi Daisuke <ajitomi@gmail.com>
2025-08-13 11:39:07 +0900

    dhkem-x25519: refine directory structure. (#608)

--- packages/dhkem-x25519/mod.ts
+++ packages/dhkem-x25519/mod.ts
@@ -1,3 +1,2 @@
-export { DhkemX25519HkdfSha256 } from "./src/dhkemX25519.ts";
+export { DhkemX25519HkdfSha256, X25519 } from "./src/dhkemX25519.ts";
 export { HkdfSha256 } from "./src/hkdfSha256.ts";
-export { X25519 } from "./src/x25519.ts";
