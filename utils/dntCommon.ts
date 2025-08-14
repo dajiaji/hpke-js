a61001d63324534578b7e2543a512d92e4fb849f
Ajitomi Daisuke <dajiaji@gmail.com>
2025-08-14 13:56:26 +0900

    base: add deno task test:browsers.

--- utils/dntCommon.ts
+++ utils/dntCommon.ts
@@ -5,6 +5,7 @@ export async function beforeBuild(name: string): Promise<void> {
   // Clean up dist
   await emptyDir(`../../npm/packages/${name}`);
   await emptyDir(`../../npm/samples/${name}`);
+  await emptyDir(`../../npm/test/${name}/runtimes/browsers`);
   await emptyDir(`../../npm/test/${name}/runtimes/cloudflare`);
 
   // Remove node_modules
@@ -17,6 +18,13 @@ export async function beforeBuild(name: string): Promise<void> {
   }
   try {
     await Deno.remove("test/runtimes/bun/node_modules", {
+      recursive: true,
+    });
+  } catch {
+    // ignore
+  }
+  try {
+    await Deno.remove("test/runtimes/browsers/node_modules", {
       recursive: true,
     });
   } catch {
@@ -36,6 +44,11 @@ export function afterBuild(name: string) {
   copySync(
     "samples/node",
     `../../npm/samples/${name}`,
+    { overwrite: true },
+  );
+  copySync(
+    "test/runtimes/browsers",
+    `../../npm/test/${name}/runtimes/browsers`,
     { overwrite: true },
   );
   copySync(
