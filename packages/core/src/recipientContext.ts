41e373175be44dfec0837f5a00beb4536523d7d2
Filip Skokan <panva.ip@gmail.com>
2025-11-13 19:34:35 +0100

    fix for GHSA-73g8-5h73-26h4

--- packages/core/src/recipientContext.ts
+++ packages/core/src/recipientContext.ts
@@ -1,17 +1,24 @@
 import { EMPTY, OpenError } from "@hpke/common";
 
 import { EncryptionContextImpl } from "./encryptionContext.ts";
+import { Mutex } from "./mutex.ts";
 
 export class RecipientContextImpl extends EncryptionContextImpl {
+  #mutex?: Mutex;
+
   override async open(
     data: ArrayBuffer,
     aad: ArrayBuffer = EMPTY.buffer as ArrayBuffer,
   ): Promise<ArrayBuffer> {
+    this.#mutex ??= new Mutex();
+    const release = await this.#mutex.lock();
     let pt: ArrayBuffer;
     try {
       pt = await this._ctx.key.open(this.computeNonce(this._ctx), data, aad);
     } catch (e: unknown) {
       throw new OpenError(e);
+    } finally {
+      release();
     }
     this.incrementSeq(this._ctx);
     return pt;
