92d5795d00a2d939ee72008a49c63a7b50568a86
Ajitomi Daisuke <dajiaji@gmail.com>
2025-11-16 15:56:05 +0900

    core: update webkit test result.

--- packages/core/test/runtimes/browsers/core.spec.ts
+++ packages/core/test/runtimes/browsers/core.spec.ts
@@ -21,24 +21,19 @@ test("standard curves test with deriveKeyPair", async ({ browserName, page }) =>
   }
 });
 
-test("secure curves test with generateKeyPair", async ({ browserName, page }) => {
+test("secure curves test with generateKeyPair", async ({ page }) => {
   await page.goto("./secureCurves.html");
   await page.click("text=run");
   await page.waitForTimeout(5000);
-  if (browserName === "webkit") {
-    await expect(page.locator("id=pass")).toHaveText("0");
-    await expect(page.locator("id=fail")).toHaveText("6");
-  } else {
-    await expect(page.locator("id=pass")).toHaveText("6");
-    await expect(page.locator("id=fail")).toHaveText("0");
-  }
+  await expect(page.locator("id=pass")).toHaveText("6");
+  await expect(page.locator("id=fail")).toHaveText("0");
 });
 
 test("secure curves test with deriveKeyPair", async ({ browserName, page }) => {
   await page.goto("./secureCurvesWithDeriveKeyPair.html");
   await page.click("text=run");
   await page.waitForTimeout(5000);
-  if (browserName === "chromium") {
+  if (browserName === "chromium" || browserName === "webkit") {
     await expect(page.locator("id=pass")).toHaveText("6");
     await expect(page.locator("id=fail")).toHaveText("0");
   } else {
