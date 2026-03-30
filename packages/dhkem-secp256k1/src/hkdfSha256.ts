f338ce4c36773395c367c0621cb21a19369a0201
Ajitomi Daisuke <ajitomi@gmail.com>
2026-03-30 11:31:31 +0900

    refactor(hkdf): share extract fallback across implementations (#748)

--- packages/dhkem-secp256k1/src/hkdfSha256.ts
+++ packages/dhkem-secp256k1/src/hkdfSha256.ts
@@ -1,28 +1,7 @@
-import { HkdfSha256Native, hmac, sha256, toArrayBuffer } from "@hpke/common";
+import { HkdfSha256Native, hmac, sha256 } from "@hpke/common";
 
 export class HkdfSha256 extends HkdfSha256Native {
-  public override async extract(
-    salt: ArrayBufferLike | ArrayBufferView,
-    ikm: ArrayBufferLike | ArrayBufferView,
-  ): Promise<ArrayBuffer> {
-    await this._setup();
-    const saltBuf = salt.byteLength === 0
-      ? new ArrayBuffer(this.hashSize)
-      : toArrayBuffer(salt);
-    const ikmBuf = toArrayBuffer(ikm);
-    if (saltBuf.byteLength !== this.hashSize) {
-      return hmac(sha256, new Uint8Array(saltBuf), new Uint8Array(ikmBuf))
-        .buffer as ArrayBuffer;
-    }
-    const key = await (this._api as SubtleCrypto).importKey(
-      "raw",
-      saltBuf,
-      this.algHash,
-      false,
-      [
-        "sign",
-      ],
-    );
-    return await (this._api as SubtleCrypto).sign("HMAC", key, ikmBuf);
+  constructor() {
+    super((salt, ikm) => hmac(sha256, salt, ikm));
   }
 }
--- packages/hpke-js/src/kdfs/hkdfSha384.ts
+++ packages/hpke-js/src/kdfs/hkdfSha384.ts
@@ -1,28 +1,7 @@
-import { HkdfSha384Native, hmac, sha384, toArrayBuffer } from "@hpke/common";
+import { HkdfSha384Native, hmac, sha384 } from "@hpke/common";
 
 export class HkdfSha384 extends HkdfSha384Native {
-  public override async extract(
-    salt: ArrayBufferLike | ArrayBufferView,
-    ikm: ArrayBufferLike | ArrayBufferView,
-  ): Promise<ArrayBuffer> {
-    await this._setup();
-    const saltBuf = salt.byteLength === 0
-      ? new ArrayBuffer(this.hashSize)
-      : toArrayBuffer(salt);
-    const ikmBuf = toArrayBuffer(ikm);
-    if (saltBuf.byteLength !== this.hashSize) {
-      return hmac(sha384, new Uint8Array(saltBuf), new Uint8Array(ikmBuf))
-        .buffer as ArrayBuffer;
-    }
-    const key = await (this._api as SubtleCrypto).importKey(
-      "raw",
-      saltBuf,
-      this.algHash,
-      false,
-      [
-        "sign",
-      ],
-    );
-    return await (this._api as SubtleCrypto).sign("HMAC", key, ikmBuf);
+  constructor() {
+    super((salt, ikm) => hmac(sha384, salt, ikm));
   }
 }
